9605a6b8dfdccaf90e2acba1ea08749d69e2d47e
Dilbar Isakova <geekwarns@gmail.com>
2022-10-17 23:56:50 +0500

    first code

---
+++ 21dCCh/21dCCh/_1dCChApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1dCChApp.swift
+//  21dCCh
+//
+//  Created by eesaack on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct _1dCChApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
